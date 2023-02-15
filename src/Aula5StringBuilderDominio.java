eb538b3435af507f801707e85b0b7e3391fdf2e6
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 20:25:11 -0300

    first commit

---
+++ src/Aula5StringBuilderDominio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class Aula5StringBuilderDominio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StringBuilder sb = new StringBuilder();
+        sb.append("Criciuma, ").append(30).append(" de Janeiro de ");
+        sb.append(2023);
+        System.out.println(sb);
+    }
+    
+}
